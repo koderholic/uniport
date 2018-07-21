6ac6900affee9a3b9c19678ed9eb8d360bb15aba
Savar <savarb@gmail.com>
2018-07-21 06:44:32 +0100

    Added checklist bucket

---
+++ buckets/checklists.go
@@ -0,0 +1,87 @@
+package buckets
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/coreos/bbolt"
+	"github.com/timshannon/bolthold"
+
+	"uniport/config"
+)
+
+//Type, Status, DocID,
+
+//Checklists ...
+type Checklists struct {
+	ID uint64
+	Title, Workflow,
+	Description string
+	Createdate,
+	Updatedate time.Time
+
+	Order, Proforma, Commercial, FormM         string
+	OrderID, ProformaID, CommercialID, FormMID uint64
+}
+
+func (Checklist Checklists) bucketName() string {
+	return "Checklists"
+}
+
+//Create ...
+func (Checklist Checklists) Create(bucketType *Checklists) (err error) {
+
+	if err = config.Get().BoltHold.Bolt().Update(func(tx *bolt.Tx) error {
+
+		if bucketType.Createdate.IsZero() {
+			bucketType.Createdate = time.Now()
+			bucketType.Updatedate = bucketType.Createdate
+		}
+
+		if bucketType.ID == 0 {
+			bucket := tx.Bucket([]byte(Checklist.bucketName()))
+			bucketType.ID, _ = bucket.NextSequence()
+			bucketType.Createdate = time.Now()
+		} else {
+			bucketType.Updatedate = time.Now()
+		}
+
+		err = config.Get().BoltHold.TxUpsert(tx, bucketType.ID, bucketType)
+		return err
+	}); err != nil {
+		log.Printf(err.Error())
+	}
+	return
+}
+
+//List ...
+func (Checklist Checklists) List() (resultsALL []string) {
+	var results []Checklists
+
+	if err := config.Get().BoltHold.Bolt().View(func(tx *bolt.Tx) error {
+		err := config.Get().BoltHold.Find(&results, bolthold.Where("ID").Gt(uint64(0)))
+		return err
+	}); err != nil {
+		log.Printf(err.Error())
+	} else {
+		for _, record := range results {
+			resultsALL = append(resultsALL, fmt.Sprintf("%+v", record))
+		}
+	}
+	return
+}
+
+//GetFieldValue ...
+func (Checklist Checklists) GetFieldValue(Field string, Value interface{}) (results []Checklists, err error) {
+
+	if len(Field) > 0 {
+		if err = config.Get().BoltHold.Bolt().View(func(tx *bolt.Tx) error {
+			err = config.Get().BoltHold.Find(&results, bolthold.Where(Field).Eq(Value).SortBy("ID").Reverse())
+			return err
+		}); err != nil {
+			log.Printf(err.Error())
+		}
+	}
+	return
+}
